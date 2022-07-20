3ac90a32313cbfc100ecdd32d730df472da11b3f
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2022-07-20 16:46:09 +0100

    Update index.js

--- strings/index.js
+++ strings/index.js
@@ -1,2 +1,8 @@
+const answer1 = document.querySelector("#answer1");
+const answer2 = document.querySelector("#answer2");
+const answer3 = document.querySelector("#answer3");
+const answer4 = document.querySelector("#answer4");
+const answer5 = document.querySelector("#answer5");
+const answer6 = document.querySelector("#answer6");
 
 
