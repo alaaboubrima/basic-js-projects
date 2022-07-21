b12602512b372e372a9106b8c7d4692200e70eef
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2022-07-21 10:31:29 +0100

    Update variations.js

--- strings/variations.js
+++ strings/variations.js
@@ -2,3 +2,7 @@ function getNumberOfChars(name) {
     // number of characters in: name
     return name.length
 }
+function getFirstChar(name) {
+    // first character of: name
+    return name[0]
+}
