90fe06d945487ee0b4d038eca146cade0f813ada
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2022-07-27 19:20:23 +0100

    Update index.js

--- 2- arrays/index.js
+++ 2- arrays/index.js
@@ -3,3 +3,12 @@ const yourGrade = document.querySelector("#your-grade");
 
 const grades = [];
 
+const renderStats2Table = grades => {
+    const tbody = document.querySelector("#stats-table2 tbody");
+    tbody.innerHTML = `<tr>
+        <td>${getPassingGrades(grades)}</td>
+        <td>${getFailingGrades(grades)}</td>
+        <td>${getRaisedGrades(grades)}</td>
+    </tr>`;
+}
+
