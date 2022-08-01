68133ad3db415db149088d02e4b1d5cf7e5e1a97
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2022-08-01 13:31:02 +0100

    Update classroom.js

--- 2- arrays/classroom.js
+++ 2- arrays/classroom.js
@@ -1,3 +1,10 @@
 function getNumberOfGrades(grades) {
     return grades.length;
 }
+function getSumGrades(grades) {
+    let sum = 0;
+    grades.forEach(function(grade) {
+        sum += grade;
+    })
+return sum;
+}
