618ca0a3146893266945288c2cd811bd804d216d
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2022-08-01 13:31:07 +0100

    Update classroom.js

--- 2- arrays/classroom.js
+++ 2- arrays/classroom.js
@@ -11,3 +11,8 @@ return sum;
 function getAverageValue(grades) {
     return getSumGrades(grades) / getNumberOfGrades(grades);
 }
+function getPassingGrades(grades) {
+    return grades.filter(function(grade) {
+        return grade >= 10
+    })
+}
